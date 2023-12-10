4ca171c80f1506a933cb272afcd62d281e1ac95f
1-cwc <2021694476@qq.com>
2023-12-10 17:52:38 +0800

    update: husky

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,28 +1,28 @@
 //对外暴露配置路由(常量路由):全部用户都可以访问到的路由
 export const constantRoute = [
-  // {
-  //   //登录成功以后展示数据的路由
-  //   path: '/',
-  //   // component: () => import('@/layout/index.vue'),
-  //   name: 'layout',
-  //   meta: {
-  //     title: '',
-  //     hidden: false,
-  //     icon: '',
-  //   },
-  //   redirect: '/home',
-  //   children: [
-  //     {
-  //       path: '/home',
-  //       // component: () => import('@/views/home/index.vue'),
-  //       meta: {
-  //         title: '首页',
-  //         hidden: false,
-  //         icon: 'HomeFilled',
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    //登录成功以后展示数据的路由
+    path: '/',
+    component: () => import('@/layout/index.vue'),
+    name: 'layout',
+    meta: {
+      title: '',
+      hidden: false,
+      icon: '',
+    },
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        // component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '首页',
+          hidden: false,
+          icon: 'HomeFilled',
+        },
+      },
+    ],
+  },
 ]
 
 //异步路由
